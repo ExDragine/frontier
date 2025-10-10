0224c391f91f011ff9bd34a29178513a2b81488c
ExDragine <ExDragine@outlook>
2025-10-10 18:25:37 +0800

    chor. fix apod, add earth now

--- plugins/clockwork/__init__.py
+++ plugins/clockwork/__init__.py
@@ -3,12 +3,12 @@ import os
 import dotenv
 import httpx
 from nonebot import require
-from nonebot_plugin_apscheduler import scheduler
 
+require("nonebot_plugin_apscheduler")
 require("nonebot_plugin_alconna")
 dotenv.load_dotenv()
-
 from nonebot_plugin_alconna import Image, Target, Text, UniMessage  # noqa: E402
+from nonebot_plugin_apscheduler import scheduler  # noqa: E402
 
 
 async def github_post_news():
@@ -35,8 +35,15 @@ async def github_post_news():
 async def apod_everyday():
     url = "https://api.nasa.gov/planetary/apod"
     params = {"api_key": os.getenv("NASA_API_KEY", "DEMO_KEY")}
-    response = httpx.get(url, params=params).json()[0]
+    response = httpx.get(url, params=params).json()
     title = f"【NASA Astronomy Picture of the Day】\n{response['title']}\n{response['explanation']}"
     image_url = response["url"]
     message = UniMessage([Text(title), Image(url=image_url)])
     await message.send(target=Target.group(os.getenv("APOD_GROUP_ID", "")))
+
+
+@scheduler.scheduled_job(trigger="cron", hour="8,12,18", minute="30")
+async def earth_now():
+    URL = "https://www.storm-chasers.cn/wp-content/uploads/satimgs/Composite_TVIS_FDLK.jpg"
+    message = UniMessage([Text("半个钟之前的地球"), Image(url=URL)])
+    await message.send(target=Target.group(os.getenv("EARTH_NOW_GROUP_ID", "")))
