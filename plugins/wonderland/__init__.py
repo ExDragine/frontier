7cb1239dbd609e4f10cb471a30d937d8ff5c4d89
ExDragine <ExDragine@outlook.com>
2026-01-16 14:33:06 +0800

    fix: paint bug

--- plugins/wonderland/__init__.py
+++ plugins/wonderland/__init__.py
@@ -1,9 +1,12 @@
 import base64
+import io
+import json
 
-from nonebot import on_command, require
+from nonebot import logger, on_command, require
 from nonebot.internal.adapter import Event
+from openai import AsyncClient
+from PIL import Image
 
-from plugins.wonderland.painter import paint
 from utils.configs import EnvConfig
 from utils.message import message_extract
 
@@ -21,21 +24,24 @@ async def handle_painter(event: Event):
     text = text.replace("/画图", "")
     if not text:
         await UniMessage.text("你想画点什么？").send()
-    with open("./prompts/system_prompt_image.txt", encoding="utf-8") as f:
-        img_sys_prompt = f.read()
-    messages = [
-        {"role": "system", "content": img_sys_prompt},
-        {
-            "role": "user",
-            "content": [{"type": "text", "text": text}]
-            + [
-                {"type": "image_url", "image_url": f"data:image/jpeg;base64,{base64.b64encode(image).decode()}"}
-                for image in images
-            ],
-        },
-    ]
-    image = await paint(messages)
+    image = await paint(text)
     if image:
         await UniMessage.image(raw=image).send()
     else:
         await UniMessage.text("这里空空如也，什么都没有画出来。").send()
+
+
+async def paint(prompt: str) -> bytes:
+    client = AsyncClient(base_url=EnvConfig.OPENAI_BASE_URL, api_key=EnvConfig.OPENAI_API_KEY.get_secret_value())
+    extra_body: dict = {"modalities": ["image"]}
+    logger.info(f"🎨 调用绘图API, extra_body: {extra_body}")
+    response = await client.chat.completions.create(
+        model=EnvConfig.PAINT_MODEL,
+        messages=[{"role": "user", "content": prompt}],
+        extra_body=extra_body,
+    )
+    messages = json.loads(response.choices[0].message.model_dump_json(indent=4))
+    image = messages.get("images")[0].get("image_url").get("url").split(",", 1)[1]
+    with io.BytesIO() as img_bytes:
+        Image.open(io.BytesIO(base64.b64decode(image))).convert("RGB").save(img_bytes, format="JPEG")
+        return img_bytes.getvalue()
