94fa9effffd06fcb5ec8db14fbf37bb527d9bfa6
ExDragine <ExDragine@outlook.com>
2026-05-08 18:34:13 +0800

    fix(playground): optimize user ID retrieval in nudge handling

--- plugins/playground/__init__.py
+++ plugins/playground/__init__.py
@@ -7,7 +7,10 @@ event = on_notice(priority=0, block=True)
 
 @event.handle()
 async def handle_message(bot: Bot, event: FriendNudgeEvent | GroupNudgeEvent):
+    user_id = int(event.get_user_id())
+    if user_id == int(bot.self_id):
+        return
     if isinstance(event, FriendNudgeEvent):
-        await bot.send_friend_nudge(user_id=int(event.get_user_id()))
+        await bot.send_friend_nudge(user_id=user_id)
     elif isinstance(event, GroupNudgeEvent):
-        await bot.send_group_nudge(group_id=event.data.group_id, user_id=int(event.get_user_id()))
+        await bot.send_group_nudge(group_id=event.data.group_id, user_id=user_id)
